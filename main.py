ce9092f745963e0244793ef4dbd1b60a62fc053b
Kasim Hussain <kasiimh1@gmail.com>
2021-10-28 19:29:06 +0100

    clean up

--- main.py
+++ main.py
@@ -1,55 +1,5 @@
-import os, plistlib, time, sys, subprocess, argparse, tssUtils, fetch, os.path
+import os, time, sys, argparse, tssUtils, fetch, os.path, utils
 from os import path
-
-def checkManifest(fileLocation, device, boardconfig, version, element, savePath):
-    fileName = os.path.expanduser(fileLocation)
-    if os.path.exists(fileName):
-        with open(fileName, 'rb') as f:
-            pl = plistlib.load(f)
-        if device in pl['SupportedProductTypes']:
-            print("[Found] %s in " % device + "BuildManifest")
-            for i in pl['BuildIdentities']:
-                if boardconfig in i['Info']['DeviceClass']:
-                    print("[Found] %s version in BuildManifest" %i['ProductMarketingVersion'])
-                    print("[Found] %s boardconfig entry in BuildManifest" %i['Info']['DeviceClass'])
-                    print("[Found] SEP at path:[%s] in BuildManifest" %i['Manifest']['SEP']['Info']['Path'])
-                    fetch.downloadFileFromIPSW(element, [i['Manifest']['SEP']['Info']['Path']], savePath)
-                    if 'iPhone' in device:
-                        print("[Found] Baseband at path:[%s] in BuildManifest" %i['Manifest']['BasebandFirmware']['Info']['Path'])
-                        fetch.downloadFileFromIPSW(element, [i['Manifest']['BasebandFirmware']['Info']['Path']], savePath)
-                    else:
-                        print("[Warning] %s does not use Baseband for restores!" %device)
-                    return
-            else:
-                print("[Error] BoardConfig: [%s] does not match entry in BuildManifest, check again!"%boardconfig)
-        else:
-            print("[Error] Counldn't match %s to entry in BuildManifest" %device)
-    else:
-        print('[Error] %s does not exist, so can\'t be read' % fileName)
-    return
-
-def deviceExtractionTool(binaryName, stripValue, grepValue, replace):
-    if sys.platform == "darwin":
-        command = binaryName + " | grep " + grepValue
-    if sys.platform == "win32":
-        command = binaryName + " | findstr " + grepValue
-    command = binaryName + " | grep " + grepValue
-    process = subprocess.Popen(command,shell=True,stdout=subprocess.PIPE,stderr=subprocess.PIPE,encoding="utf8")
-    output = process.communicate()
-    stdOutput, stdErrValue = output
-    stdOutput = stdOutput.strip()
-    stdOutput = stdOutput[stripValue:]
-    if replace == True:
-        stdOutput = stdOutput.replace(" ", "")
-    return dataReturn(stdOutput, stdErrValue)
-
-def dataReturn(output, error):
-    ret = None
-    if error != None:
-        ret = error
-    if output != None:
-        ret = output
-    return ret
 
 def main():
     parser = argparse.ArgumentParser(description="FutureHelper: FutureRestore SEP, Basband and BuildManifest.plist Downloader by Kasiimh1")
@@ -81,20 +31,20 @@ def main():
         time.sleep(1)
         try:
             if sys.platform == "darwin":
-                udid = deviceExtractionTool("ideviceinfo", 16, "UniqueDeviceID: ", False)
-                ecid = deviceExtractionTool("ideviceinfo", 13, "UniqueChipID: ", True)
-                platform = deviceExtractionTool("ideviceinfo", 18, "HardwarePlatform: ", False)
-                product = deviceExtractionTool("ideviceinfo", 13, "ProductType: ", False)
-                user = deviceExtractionTool("ideviceinfo", 12, "DeviceName: ", False)
-                boardid = deviceExtractionTool("ideviceinfo", 15, "HardwareModel: ", False)
+                udid = utils.deviceExtractionTool("ideviceinfo", 16, "UniqueDeviceID: ", False)
+                ecid = utils.deviceExtractionTool("ideviceinfo", 13, "UniqueChipID: ", True)
+                platform = utils.deviceExtractionTool("ideviceinfo", 18, "HardwarePlatform: ", False)
+                product = utils.deviceExtractionTool("ideviceinfo", 13, "ProductType: ", False)
+                user = utils.deviceExtractionTool("ideviceinfo", 12, "DeviceName: ", False)
+                boardid = utils.deviceExtractionTool("ideviceinfo", 15, "HardwareModel: ", False)
             if sys.platform == "win32":
                 os.chdir(bundle_dir + "/win32/")
-                udid = deviceExtractionTool("ideviceinfo", 16, "UniqueDeviceID: ", False)
-                ecid = deviceExtractionTool("ideviceinfo", 13, "UniqueChipID: ", True)
-                platform = deviceExtractionTool("ideviceinfo", 18, "HardwarePlatform: ", False)
-                product = deviceExtractionTool("ideviceinfo", 13, "ProductType: ", False)
-                user = deviceExtractionTool("ideviceinfo", 12, "DeviceName: ", False)
-                boardid = deviceExtractionTool("ideviceinfo", 15, "HardwareModel: ", False)
+                udid = utils.deviceExtractionTool("ideviceinfo", 16, "UniqueDeviceID: ", False)
+                ecid = utils.deviceExtractionTool("ideviceinfo", 13, "UniqueChipID: ", True)
+                platform = utils.deviceExtractionTool("ideviceinfo", 18, "HardwarePlatform: ", False)
+                product = utils.deviceExtractionTool("ideviceinfo", 13, "ProductType: ", False)
+                user = utils.deviceExtractionTool("ideviceinfo", 12, "DeviceName: ", False)
+                boardid = utils.deviceExtractionTool("ideviceinfo", 15, "HardwareModel: ", False)
 
             print("[*] Fetching Infromation From Device")
             print("-- Device Information --")
@@ -110,7 +60,7 @@ def main():
                 for index, element in enumerate(tssUtils.ipswGrabber(product, i, args.b)):
                     fetch.downloadFileFromIPSW(element['url'], ["BuildManifest.plist"], args.s + "%s/" %product + "%s/" %i)
                     print("-- Performing BuildManifest Lookup for %s --" %product)
-                    checkManifest(args.s + "%s/" %product + "%s/" %i + "BuildManifest.plist", product, boardid.lower() , i, element['url'], args.s + "%s/" %product + "%s/" %i)
+                    utils.checkManifest(args.s + "%s/" %product + "%s/" %i + "BuildManifest.plist", product, boardid.lower(), element['url'], args.s + "%s/" %product + "%s/" %i)
         except:
             print("Something went wrong, Connect Device again and run script again!")
             sys.exit(-1)
--- utils.py
+++ utils.py
@@ -1,40 +1,56 @@
-import os, csv
-
-def createSavePath(ecid, savePath, version):
-    if not os.path.isdir(os.path.expanduser(savePath + '/SaveMe-Tickets/' + ecid + '/')):
-        try:
-            os.mkdir(os.path.expanduser(savePath + '/SaveMe-Tickets/' + ecid + '/'))
-        except FileExistsError:
-            print('[*] Skipping creating ECID folder as %s it already exists' %ecid)
-    if not os.path.isdir(os.path.expanduser(savePath + '/SaveMe-Tickets/' + ecid + '/') + version):
-        try:
-            os.mkdir(os.path.expanduser(savePath + '/SaveMe-Tickets/' + ecid + '/' + version))
-        except FileExistsError:
-            print('[*] Skipping creating iOS version folder as %s it already exists' %ecid)
+import os, plistlib, sys, subprocess, fetch, os.path
 
 def openFolder(savePath):
     print('[*] File should be in:', savePath)
     os.system('open ' + savePath)
     #subprocess.run(['explorer', os.path.realpath(args.path + 'SaveMe-Tickets/' + i['ecid'] + '/' + version + '/' )])
 
-def writeDevicesToOutput(data, path):
-    f = open(path + "/SaveMe-Devices", "a")
-    f.write(data)
-    f.close()
+def checkManifest(fileLocation, device, boardconfig, element, savePath):
+    fileName = os.path.expanduser(fileLocation)
+    if os.path.exists(fileName):
+        with open(fileName, 'rb') as f:
+            pl = plistlib.load(f)
+        if device in pl['SupportedProductTypes']:
+            print("[Found] %s in " % device + "BuildManifest")
+            for i in pl['BuildIdentities']:
+                if boardconfig in i['Info']['DeviceClass']:
+                    print("[Found] %s version in BuildManifest" %i['ProductMarketingVersion'])
+                    print("[Found] %s boardconfig entry in BuildManifest" %i['Info']['DeviceClass'])
+                    print("[Found] SEP at path:[%s] in BuildManifest" %i['Manifest']['SEP']['Info']['Path'])
+                    fetch.downloadFileFromIPSW(element, [i['Manifest']['SEP']['Info']['Path']], savePath)
+                    if 'iPhone' in device:
+                        print("[Found] Baseband at path:[%s] in BuildManifest" %i['Manifest']['BasebandFirmware']['Info']['Path'])
+                        fetch.downloadFileFromIPSW(element, [i['Manifest']['BasebandFirmware']['Info']['Path']], savePath)
+                    else:
+                        print("[Warning] %s does not use Baseband for restores!" %device)
+                    return
+            else:
+                print("[Error] BoardConfig: [%s] does not match entry in BuildManifest, check again!"%boardconfig)
+        else:
+            print("[Error] Counldn't match %s to entry in BuildManifest" %device)
+    else:
+        print('[Error] %s does not exist, so can\'t be read' % fileName)
+    return
 
-def printCachedDevices(savePath):
-    file = os.path.expanduser(savePath + '/SaveMe-Tickets/SaveMe-Devices')
-    with open(file, mode='r') as csv_file:
-        csv_reader = csv.DictReader(csv_file)
-        for i in csv_reader:
-            print("------------------------------------------------------------------------")
-            print("-- Found Device --")
-            print("[D] Name: " + i['name'])
-            print("[D] Model: " + i['model'])
-            print("[D] UDID: " + i['udid'])
-            print("[D] ECID: " + i['ecid'])
-            print("[D] BoardID: " + i['boardid'])
-            print("[D] Platform: " + i['platform'])
-            print("[D] Generator: " + i['generator'])
-            print("[D] APNonce: " + i['apnonce'])
-        print("------------------------------------------------------------------------")
+def deviceExtractionTool(binaryName, stripValue, grepValue, replace):
+    if sys.platform == "darwin":
+        command = binaryName + " | grep " + grepValue
+    if sys.platform == "win32":
+        command = binaryName + " | findstr " + grepValue
+    command = binaryName + " | grep " + grepValue
+    process = subprocess.Popen(command,shell=True,stdout=subprocess.PIPE,stderr=subprocess.PIPE,encoding="utf8")
+    output = process.communicate()
+    stdOutput, stdErrValue = output
+    stdOutput = stdOutput.strip()
+    stdOutput = stdOutput[stripValue:]
+    if replace == True:
+        stdOutput = stdOutput.replace(" ", "")
+    return dataReturn(stdOutput, stdErrValue)
+
+def dataReturn(output, error):
+    ret = None
+    if error != None:
+        ret = error
+    if output != None:
+        ret = output
+    return ret
